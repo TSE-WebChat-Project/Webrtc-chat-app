ef33ada0e938cb3bfbc77acbdbac767a1c5ee667
Cabe-Towers <cabe.towers@gmail.com>
2023-05-09 04:04:56 +0100

    installed firebase

---
+++ src/WebRTC.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1wUl4ZcZ952raQlVfDkai8rABctx00VA",
+  authDomain: "resonant-rock-376300.firebaseapp.com",
+  projectId: "resonant-rock-376300",
+  storageBucket: "resonant-rock-376300.appspot.com",
+  messagingSenderId: "504674438089",
+  appId: "1:504674438089:web:a4bf32a447a88534b4f618",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
